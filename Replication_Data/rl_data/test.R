f5904a397a7a17bbfbc40001ad6cd42e265d37dc
s2518809 <navarrenicolas@gmail.com>
2023-11-09 16:54:14 +0000

    A2C rewards only

--- Replication_Data/rl_data/test.R
+++ Replication_Data/rl_data/test.R
@@ -8,7 +8,7 @@ multmerge <- function() {
     for(i in 1:length(filenames)){
         df <- read.csv(file = filenames[i], header= TRUE)
         df <- df %>% mutate(condition = filenames[i])
-        df <- df %>% mutate(condition = recode(condition,"sl_290024.csv"="both_loo", "sl_290396.csv"="none-PPO2", "sl_290464.csv"="none", "sl_290495.csv"="disjunctive_train", "sl_290496.csv"="conjunctive_train", "sl_290529.csv"="disjunctive_loo", "sl_290530.csv"="conjunctive_loo"))
+        df <- df %>% mutate(condition = recode(condition,"sl_290531.csv"="both_loo", "sl_290396.csv"="none-PPO2", "sl_290464.csv"="none", "sl_290495.csv"="disjunctive_train", "sl_290496.csv"="conjunctive_train", "sl_290529.csv"="disjunctive_loo", "sl_290530.csv"="conjunctive_loo"))
 # very high chance that the both_loo is something else
         colnames(df)%>%print
         datalist <- rbind(datalist,df)
