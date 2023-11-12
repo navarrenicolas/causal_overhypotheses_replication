231eb183629388d736507c2f1f558c714327d6f3
s2518809 <navarrenicolas@gmail.com>
2023-11-12 21:14:00 +0000

    modifying data

--- Replication_Data/rl_data/test.R
+++ Replication_Data/rl_data/test.R
@@ -9,10 +9,10 @@ multmerge <- function() {
         df <- read.csv(file = filenames[i], header= TRUE)
         df <- df %>% mutate(condition = filenames[i])
         df <- df %>% mutate(condition = recode(condition,"sl_290531.csv"="both_loo", "sl_290396.csv"="none-PPO2", "sl_290464.csv"="none", "sl_290495.csv"="disjunctive_train", "sl_290496.csv"="conjunctive_train", "sl_290529.csv"="disjunctive_loo", "sl_290530.csv"="conjunctive_loo"))
-# very high chance that the both_loo is something else
+        #df <- df[df$condition != "sl_291271.csv",]
+    # very high chance that the both_loo is something else
         colnames(df)%>%print
         datalist <- rbind(datalist,df)
-        #datalist[i] <- df
     }
     return(datalist)
 }
@@ -20,7 +20,7 @@ multmerge <- function() {
 dlist <- multmerge()
 
 ggplot()+ 
-    geom_path(data=subset(dlist,condition!="none-PPO2"), aes(x = steps, y = reward,colour=condition)) +
-    theme_minimal()
-
+    geom_path(data=subset(dlist,condition!="none-PPO2" & condition != "sl_291271.csv"), aes(x = steps, y = reward,colour=condition)) +
+    theme_minimal()+
+    theme(text = element_text(family = "Times"))
 
